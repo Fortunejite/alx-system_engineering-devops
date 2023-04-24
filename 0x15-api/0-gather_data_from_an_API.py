12c3505adbcde50f52cd607536770d142a98e973
Fortunejite <111015282+Fortunejite@users.noreply.github.com>
2023-04-24 17:37:17 +0100

    B

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,30 +1,14 @@
 #!/usr/bin/python3
-"""Gets the TODO lists of an employee in a predefines url"""
-
+"""Returns to-do list information for a given employee ID."""
 import requests
 import sys
 
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("Error: Please provide an employee ID as a command-line argument.")
-        sys.exit(1)
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    try:
-        id = int(sys.argv[1])
-    except ValueError:
-        print("Error: Employee ID must be an integer.")
-        sys.exit(1)
-
-    user_uri = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
-    todo_uri = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(id)
-    user = requests.get(user_uri).json()
-    name = user['name']
-    todos = requests.get(todo_uri).json()
-
-    completed = [i for i in todos if i['completed']]
-    out = 'Employee {} is done with tasks({}/{}):'.format(name, len(completed), len(todos))
-    print(out)
-
-    for i in completed:
-        title = i.get('title')
-        print('\t {}'.format(title))
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
