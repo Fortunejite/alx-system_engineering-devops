d9ad59fd9889be7ab093a4eb7698c2750a1c0797
Fortunejite <111015282+Fortunejite@users.noreply.github.com>
2023-04-24 17:47:05 +0100

    M

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,35 +1,18 @@
 #!/usr/bin/python3
 """Exports to-do list information for a given employee ID to CSV format."""
+import csv
+import requests
+import sys
 
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-if __name__ == '__main__':
-    import requests
-    import sys
-    import json
-    import csv
-
-    id = sys.argv[1]
-    id = int(id)
-    todo = []
-    uri = f'https://jsonplaceholder.typicode.com/users/{id}'
-    result = requests.get(uri)
-    result = result.text
-    result = json.loads(result)
-    name = result['username']
-    uri = f'https://jsonplaceholder.typicode.com/todos/'
-    result = requests.get(uri)
-    result = result.text
-    result = json.loads(result)
-    for i in result:
-        if i['userId'] == id:
-            todo.append(i)
-
-    field = ['userId', 'username', 'completed', 'title']
-    for i in todo:
-        i['username'] = name
-        i['userId'] = str(id)
-        del i['id']
-    with open(f'{id}.csv', 'w', newline='') as fi:
-        writer = csv.DictWriter(fi, fieldnames=field, quoting=csv.QUOTE_ALL)
-        for row in todo:
-            writer.writerow(row)
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todos]
